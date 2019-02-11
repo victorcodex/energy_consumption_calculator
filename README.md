55f1b9e72086d0e4b64e70d97910c64251df022d
Victor <vifeanyi33@gmail.com>
2019-02-11 21:24:05 +0100

    modified ReadMe file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # basic-energy-consumption-app
 A basic demo energy consumption calculator application built with React and Node.js(Express).
 
-### `npm install`
+### `npm install` (repeat in client folder)
 ### `npm run client-install`
 ### `npm run dev`
--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.get('*', (req,res) =>{
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8000
 
 app.listen(port, () => console.log(`Server running on PORT ${port}`));
